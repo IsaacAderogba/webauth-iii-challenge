f15ffb7a090e1255b56e638335426fd11d5f9114
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-24 12:29:17 +0100

    Create test endpoint to validate everything is setup right

--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const api = require('../services');
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(api);
 
 module.exports = server;
---
+++ services/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+router.get('/api', (req, res, next) => {
+  try {
+    res.status(200).json({ message: "Api is up and running" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message, stack: err.stack })
+})
+
+module.exports = router;
